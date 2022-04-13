e4293b73d57de48a9f104571d458d7fa29ef2325
Cameron Keetch <keetchc@byu.edu>
2022-04-13 15:39:48 -0600

    Updates

---
+++ precache-manifest.5723b41b4e99f89596925eea4c61f198.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8cead3b914b7aa8bce38c8eed08dc3",
+    "url": "/LiveNbaScores/index.html"
+  },
+  {
+    "revision": "57e78468f2ad779f2e69",
+    "url": "/LiveNbaScores/static/css/2.1a7e3916.chunk.css"
+  },
+  {
+    "revision": "b910593165a000b584a3",
+    "url": "/LiveNbaScores/static/css/main.79f4bb62.chunk.css"
+  },
+  {
+    "revision": "57e78468f2ad779f2e69",
+    "url": "/LiveNbaScores/static/js/2.1d85a3a5.chunk.js"
+  },
+  {
+    "revision": "c1bdf306936ddd82567a345834b1f9d5",
+    "url": "/LiveNbaScores/static/js/2.1d85a3a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b910593165a000b584a3",
+    "url": "/LiveNbaScores/static/js/main.0de9f184.chunk.js"
+  },
+  {
+    "revision": "3c3692a33b049cff43fd",
+    "url": "/LiveNbaScores/static/js/runtime-main.1c56b060.js"
+  }
+]);
